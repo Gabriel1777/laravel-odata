d075847a898dbf309685574ba51dcfbb1c4e41e4
Luis Gabriel <lgcastellanos76@misena.edu.co>
2022-10-01 21:30:07 -0500

    update serializer

--- src/Serializers/NoDataSerializer.php
+++ src/Serializers/NoDataSerializer.php
@@ -6,7 +6,7 @@ use League\Fractal\Serializer\DataArraySerializer;
 
 class NoDataSerializer extends DataArraySerializer
 {
-    public function mergeIncludes($transformedData, $includedData)
+    public function mergeIncludes($transformedData, $includedData) : array
     {
         $includedData = array_map(function ($include) {
             return $include['data'];
